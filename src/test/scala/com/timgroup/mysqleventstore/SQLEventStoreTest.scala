538d2412e3a7d4fe7598d059e10f10dcd1f1832b
Justin Heister <justin.heister@youdevise.com>
2015-03-19 10:48:53 +0000

    Refactor test to test through EventStore interface

--- src/test/scala/com/timgroup/mysqleventstore/SQLEventStoreTest.scala
+++ src/test/scala/com/timgroup/mysqleventstore/SQLEventStoreTest.scala
@@ -12,11 +12,13 @@ class SQLEventStoreTest extends FunSpec with MustMatchers {
     override def getConnection(): Connection = ???
   }, now = () => effectiveTimestamp)
 
-  it("can save events") {
-    inTransaction { conn =>
-      eventStore.saveEventsToDB(conn, serialized(ExampleEvent(21), ExampleEvent(22)))
+  case class SingleTransactionProvider(getConnection: Connection) extends ConnectionProvider
 
-      val all = eventStore.fetchEventsFromDB(conn).events.toList
+  it("can save events") {
+    inTransaction { eventStore =>
+      eventStore.save(serialized(ExampleEvent(21), ExampleEvent(22)))
+
+      val all = eventStore.fromAll().events.toList
 
       all.map(_.effectiveTimestamp) must be(List(effectiveTimestamp, effectiveTimestamp))
       all.map(_.eventData).map(deserialize) must be(List(ExampleEvent(21),ExampleEvent(22)))
@@ -24,70 +26,70 @@ class SQLEventStoreTest extends FunSpec with MustMatchers {
   }
 
   it("can replay events from a given version number onwards") {
-    inTransaction { conn =>
-      eventStore.saveEventsToDB(conn, serialized(ExampleEvent(1), ExampleEvent(2)))
+    inTransaction { eventStore =>
+      eventStore.save(serialized(ExampleEvent(1), ExampleEvent(2)))
 
-      val previousVersion = eventStore.fetchEventsFromDB(conn).events.toList.last.version
+      val previousVersion = eventStore.fromAll().events.toList.last.version
 
-      eventStore.saveEventsToDB(conn, serialized(ExampleEvent(3), ExampleEvent(4)))
+      eventStore.save(serialized(ExampleEvent(3), ExampleEvent(4)))
 
-      val nextEvents = eventStore.fetchEventsFromDB(conn, version = previousVersion).events.toList.map(_.eventData).map(deserialize)
+      val nextEvents = eventStore.fromAll(version = previousVersion).events.toList.map(_.eventData).map(deserialize)
 
       nextEvents must be(List(ExampleEvent(3), ExampleEvent(4)))
     }
   }
 
   it("returns no events if there are none past the specified version") {
-    inTransaction { conn =>
-      eventStore.fetchEventsFromDB(conn, version = 900000).isEmpty must be(true)
+    inTransaction { eventStore =>
+      eventStore.fromAll(version = 900000).isEmpty must be(true)
     }
   }
 
   it("labels the last event of the stream") {
-    inTransaction { conn =>
-      eventStore.saveEventsToDB(conn, serialized(ExampleEvent(1), ExampleEvent(2)))
+    inTransaction { eventStore =>
+      eventStore.save(serialized(ExampleEvent(1), ExampleEvent(2)))
 
-      val nextEvents = eventStore.fetchEventsFromDB(conn).events.toList
+      val nextEvents = eventStore.fromAll().events.toList
 
       nextEvents.map(_.last) must be(List(false, true))
     }
   }
 
   it("writes events to the current version of the stream when no expected version is specified") {
-    inTransaction { conn =>
-      eventStore.saveEventsToDB(conn, serialized(ExampleEvent(1), ExampleEvent(2)))
-      eventStore.saveEventsToDB(conn, serialized(ExampleEvent(3)))
+    inTransaction { eventStore =>
+      eventStore.save(serialized(ExampleEvent(1), ExampleEvent(2)))
+      eventStore.save(serialized(ExampleEvent(3)))
 
-      eventStore.fetchEventsFromDB(conn, version = 2).events.toList.map(_.eventData).map(deserialize) must be(List(ExampleEvent(3)))
+      eventStore.fromAll(version = 2).events.toList.map(_.eventData).map(deserialize) must be(List(ExampleEvent(3)))
     }
   }
 
   it("throws OptimisticConcurrencyFailure when stream has already moved beyond the expected version") {
-    inTransaction { conn =>
-      eventStore.saveEventsToDB(conn, serialized(ExampleEvent(1), ExampleEvent(2)))
+    inTransaction { eventStore =>
+      eventStore.save(serialized(ExampleEvent(1), ExampleEvent(2)))
       intercept[OptimisticConcurrencyFailure] {
-        eventStore.saveEventsToDB(conn, serialized(ExampleEvent(3)), expectedVersion = Some(1))
+        eventStore.save(serialized(ExampleEvent(3)), expectedVersion = Some(1))
       }
     }
   }
 
   it("throws OptimisticConcurrencyFailure when stream is not yet at the expected version") {
-    inTransaction { conn =>
-      eventStore.saveEventsToDB(conn, serialized(ExampleEvent(1), ExampleEvent(2)))
+    inTransaction { eventStore =>
+      eventStore.save(serialized(ExampleEvent(1), ExampleEvent(2)))
       intercept[OptimisticConcurrencyFailure] {
-        eventStore.saveEventsToDB(conn, serialized(ExampleEvent(3)), expectedVersion = Some(10))
+        eventStore.save(serialized(ExampleEvent(3)), expectedVersion = Some(10))
       }
     }
   }
 
   it("throws OptimisticConcurrencyFailure when stream moves past expected version during save") {
     try {
-      inTransaction { conn =>
-        eventStore.fetchEventsFromDB(conn)
+      inTransaction { eventStore =>
+        eventStore.fromAll()
 
         unrelatedSavesOfEventHappens()
         intercept[OptimisticConcurrencyFailure] {
-          eventStore.saveEventsToDB(conn, serialized(ExampleEvent(3)), expectedVersion = Some(0))
+          eventStore.save(serialized(ExampleEvent(3)), expectedVersion = Some(0))
         }
       }
     } finally {
@@ -96,49 +98,47 @@ class SQLEventStoreTest extends FunSpec with MustMatchers {
   }
 
   it("returns nothing if eventstore is empty") {
-    inTransaction { conn =>
-      eventStore.fetchEventsFromDB(conn).eventData.toList must be(Nil)
+    inTransaction { eventStore =>
+      eventStore.fromAll().eventData.toList must be(Nil)
     }
   }
 
 
   it("returns only number of events asked for in batchSize") {
-    inTransaction { conn =>
-      eventStore.saveEventsToDB(conn, serialized(ExampleEvent(1), ExampleEvent(2), ExampleEvent(3), ExampleEvent(4)))
+    inTransaction { eventStore =>
+      eventStore.save(serialized(ExampleEvent(1), ExampleEvent(2), ExampleEvent(3), ExampleEvent(4)))
 
-      eventStore.fetchEventsFromDB(conn, 0, Some(2)).events.toList.map(_.eventData).map(deserialize) must be(List(ExampleEvent(1), ExampleEvent(2)))
-      eventStore.fetchEventsFromDB(conn, 2, Some(2)).events.toList.map(_.eventData).map(deserialize) must be(List(ExampleEvent(3), ExampleEvent(4)))
+      eventStore.fromAll(0, Some(2)).events.toList.map(_.eventData).map(deserialize) must be(List(ExampleEvent(1), ExampleEvent(2)))
+      eventStore.fromAll(2, Some(2)).events.toList.map(_.eventData).map(deserialize) must be(List(ExampleEvent(3), ExampleEvent(4)))
     }
   }
 
   def cleanup(): Unit = {
-    inTransaction { conn =>
-      conn.prepareStatement("delete from Event").execute()
-      conn.commit()
-    }
+    val conn = connect()
+    conn.prepareStatement("delete from Event").execute()
+    conn.close()
   }
 
   def unrelatedSavesOfEventHappens(): Unit = {
-    inTransaction { conn =>
-      eventStore.saveEventsToDB(conn, serialized(ExampleEvent(3)))
-      conn.commit()
-    }
+    new SQLEventStore(new ConnectionProvider {
+      override def getConnection(): Connection = connect()
+    }).save(serialized(ExampleEvent(3)))
   }
 
   case class ExampleEvent(a: Int)
 
-  def serialized(evts: ExampleEvent*) = evts.map(serialize).map(EventAtAtime(effectiveTimestamp, _))
+  def serialized(evts: ExampleEvent*) = evts.map(serialize)
 
   def serialize(evt: ExampleEvent) = EventData(evt.getClass.getSimpleName, evt.a.toString.getBytes("UTF-8"))
 
   def deserialize(evt: EventData) = ExampleEvent(new String(evt.body, "UTF-8").toInt)
 
-  def inTransaction[T](f: Connection => T): T = {
+  def inTransaction[T](f: EventStore => T): T = {
     val connection: Connection = connect()
 
     try {
       connection.setAutoCommit(false)
-      f(connection)
+      f(new SQLEventStore(SingleTransactionProvider(connection), now = () => effectiveTimestamp))
     } finally {
       connection.rollback()
     }
