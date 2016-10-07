225d916b38a9aaab5597f921e7c5b80926d05bfc
Steve Haslam <steve.haslam@timgroup.com>
2016-10-07 01:54:29 +0100

    SteveH: rename EventWriter to correspond to EventStreamReader interface

--- api/src/main/java/com/timgroup/eventstore/api/EventStreamWriter.java
+++ api/src/main/java/com/timgroup/eventstore/api/EventStreamWriter.java
@@ -2,7 +2,7 @@ package com.timgroup.eventstore.api;
 
 import java.util.Collection;
 
-public interface EventWriter {
+public interface EventStreamWriter {
     void write(StreamId streamId, Collection<NewEvent> events);
 
     void write(StreamId streamId, Collection<NewEvent> events, int expectedVersion);
