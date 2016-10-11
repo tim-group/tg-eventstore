6cc0cb328acf0b1111971b04cbc046bd4a1d404e
Steve Haslam <steve.haslam@timgroup.com>
2016-10-11 13:23:40 +0100

    Revert "SteveH: remove 'default' from one EventHandler method so it must be implemented"
    This reverts commit c568b48e2f66848221c8df833776929849372a9c.

--- subscription/src/main/java/com/timgroup/eventsubscription/EventHandler.java
+++ subscription/src/main/java/com/timgroup/eventsubscription/EventHandler.java
@@ -4,7 +4,7 @@ import com.timgroup.eventstore.api.Position;
 import org.joda.time.DateTime;
 
 public interface EventHandler<T> {
-    void apply(T deserialized);
+    default void apply(T deserialized) {}
 
     default void apply(Position position, DateTime timestamp, T deserialized, boolean endOfBatch) {
         apply(deserialized);
