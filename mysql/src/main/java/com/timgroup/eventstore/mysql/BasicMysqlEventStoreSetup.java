6934b173906519b9361f638c897d759068ca3dd9
Steve Haslam <steve.haslam@timgroup.com>
2016-10-12 22:39:24 +0100

    SteveH: support creating event table if not already present

--- mysql/src/main/java/com/timgroup/eventstore/mysql/BasicMysqlEventStoreSetup.java
+++ mysql/src/main/java/com/timgroup/eventstore/mysql/BasicMysqlEventStoreSetup.java
@@ -40,4 +40,23 @@ public class BasicMysqlEventStoreSetup {
             throw new RuntimeException(e);
         }
     }
+
+    public void lazyCreate() {
+        try (Connection connection = connectionProvider.getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.execute("create table if not exists " + tableName + "(" +
+                    "position bigint primary key, " +
+                    "timestamp datetime not null, " +
+                    "stream_category varchar(255) not null, " +
+                    "stream_id varchar(255) not null, " +
+                    "event_number bigint not null, " +
+                    "event_type varchar(255) not null," +
+                    "data blob not null, " +
+                    "metadata blob not null," +
+                    "unique(stream_category, stream_id, event_number)" +
+                    ")");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
