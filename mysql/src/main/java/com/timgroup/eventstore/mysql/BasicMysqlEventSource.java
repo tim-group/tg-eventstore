3f22e37b89a02a42996f03f75384e29df8f1a44d
Steve Haslam <steve.haslam@timgroup.com>
2016-11-11 15:41:24 +0000

    SteveH: provide pooled event source as a subclass that implements a close() method to shut down the pool

--- mysql/src/main/java/com/timgroup/eventstore/mysql/BasicMysqlEventSource.java
+++ mysql/src/main/java/com/timgroup/eventstore/mysql/BasicMysqlEventSource.java
@@ -1,5 +1,7 @@
 package com.timgroup.eventstore.mysql;
 
+import java.util.Collection;
+
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import com.timgroup.eventstore.api.EventCategoryReader;
 import com.timgroup.eventstore.api.EventReader;
@@ -11,8 +13,6 @@ import com.timgroup.tucker.info.Component;
 import com.timgroup.tucker.info.component.DatabaseConnectionComponent;
 import com.typesafe.config.Config;
 import org.slf4j.LoggerFactory;
-
-import java.util.Collection;
 
 import static java.lang.String.format;
 import static java.util.Collections.singletonList;
@@ -71,8 +71,7 @@ public class BasicMysqlEventSource implements EventSource {
         return singletonList(new DatabaseConnectionComponent(label, label, connectionProvider::getConnection));
     }
 
-
-    public static BasicMysqlEventSource pooledMasterDbEventSource(Config config, String tableName, String name) {
+    public static PooledMysqlEventSource pooledMasterDbEventSource(Config config, String tableName, String name) {
         ComboPooledDataSource dataSource = new ComboPooledDataSource();
         dataSource.setJdbcUrl(format("jdbc:mysql://%s:%d/%s?rewriteBatchedStatements=true",
                 config.getString("hostname"),
@@ -93,6 +92,20 @@ public class BasicMysqlEventSource implements EventSource {
             LoggerFactory.getLogger(BasicMysqlEventSource.class).warn("Failed to ensure ES schme is created", e);
         }
 
-        return new BasicMysqlEventSource(dataSource::getConnection, tableName, DefaultBatchSize, name);
+        return new PooledMysqlEventSource(dataSource, tableName, DefaultBatchSize, name);
+    }
+
+    public static final class PooledMysqlEventSource extends BasicMysqlEventSource implements AutoCloseable {
+        private final ComboPooledDataSource dataSource;
+
+        public PooledMysqlEventSource(ComboPooledDataSource dataSource, String tableName, int defaultBatchSize, String name) {
+            super(dataSource::getConnection, tableName, defaultBatchSize, name);
+            this.dataSource = dataSource;
+        }
+
+        @Override
+        public void close() {
+            dataSource.close();
+        }
     }
 }
