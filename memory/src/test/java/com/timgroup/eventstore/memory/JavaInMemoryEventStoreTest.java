54f144dd943828f63505a5e3ac2918b3d4e3c135
Steve Haslam <steve.haslam@timgroup.com>
2017-01-23 12:35:34 +0000

    SteveH: simple tests for ordering produced by position codecs

--- memory/src/test/java/com/timgroup/eventstore/memory/JavaInMemoryEventStoreTest.java
+++ memory/src/test/java/com/timgroup/eventstore/memory/JavaInMemoryEventStoreTest.java
@@ -1,9 +1,17 @@
 package com.timgroup.eventstore.memory;
 
+import java.time.Clock;
+import java.util.Arrays;
+import java.util.List;
+
 import com.timgroup.eventstore.api.EventSource;
 import com.timgroup.eventstore.api.JavaEventStoreTest;
+import com.timgroup.eventstore.api.Position;
+import org.junit.Test;
 
-import java.time.Clock;
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 public class JavaInMemoryEventStoreTest extends JavaEventStoreTest {
 
@@ -12,5 +20,26 @@ public class JavaInMemoryEventStoreTest extends JavaEventStoreTest {
     @Override
     public EventSource eventSource() {
         return new InMemoryEventSource(eventStore);
+    }
+
+
+    @Test
+    public void orders_positions_numerically() throws Exception {
+        List<Position> positions = Arrays.asList(
+                position(10L),
+                position(2L),
+                position(1L),
+                position(100L)
+        );
+        assertThat(positions.stream().sorted(eventStore::comparePositions).collect(toList()), equalTo(Arrays.asList(
+                position(1L),
+                position(2L),
+                position(10L),
+                position(100L)
+        )));
+    }
+
+    private Position position(long n) {
+        return eventStore.deserializePosition(Long.toString(n));
     }
 }
--- mysql/src/test/java/com/timgroup/eventstore/mysql/BasicMysqlPositionCodecTest.java
+++ mysql/src/test/java/com/timgroup/eventstore/mysql/BasicMysqlPositionCodecTest.java
@@ -1,7 +1,12 @@
 package com.timgroup.eventstore.mysql;
 
+import java.util.Arrays;
+import java.util.List;
+
+import com.timgroup.eventstore.api.Position;
 import org.junit.Test;
 
+import static java.util.stream.Collectors.toList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -14,5 +19,21 @@ public class BasicMysqlPositionCodecTest {
     @Test
     public void deserializes_position_from_string() throws Exception {
         assertThat(new BasicMysqlPositionCodec().deserializePosition("12345"), equalTo(new BasicMysqlEventStorePosition(12345L)));
+    }
+
+    @Test
+    public void orders_positions_numerically() throws Exception {
+        List<Position> positions = Arrays.asList(
+                new BasicMysqlEventStorePosition(10L),
+                new BasicMysqlEventStorePosition(2L),
+                new BasicMysqlEventStorePosition(1L),
+                new BasicMysqlEventStorePosition(100L)
+        );
+        assertThat(positions.stream().sorted(new BasicMysqlPositionCodec()::comparePositions).collect(toList()), equalTo(Arrays.asList(
+                new BasicMysqlEventStorePosition(1L),
+                new BasicMysqlEventStorePosition(2L),
+                new BasicMysqlEventStorePosition(10L),
+                new BasicMysqlEventStorePosition(100L)
+        )));
     }
 }
