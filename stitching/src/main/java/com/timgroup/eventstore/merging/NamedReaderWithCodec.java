e78d985f7ffd53e7f69e6a7d667c5f378cf3875e
Tom Denley <tom.denley@timgroup.com>
2017-02-06 16:18:56 +0000

    tomd: wire in position codecs

--- stitching/src/main/java/com/timgroup/eventstore/merging/NamedReaderWithCodec.java
+++ stitching/src/main/java/com/timgroup/eventstore/merging/NamedReaderWithCodec.java
@@ -1,6 +1,7 @@
 package com.timgroup.eventstore.merging;
 
 import com.timgroup.eventstore.api.EventReader;
+import com.timgroup.eventstore.api.EventSource;
 import com.timgroup.eventstore.api.PositionCodec;
 
 public final class NamedReaderWithCodec {
@@ -12,6 +13,10 @@ public final class NamedReaderWithCodec {
         this.name = name;
         this.reader = reader;
         this.codec = codec;
+    }
+
+    public static NamedReaderWithCodec fromEventSource(String name, EventSource source) {
+        return new NamedReaderWithCodec(name, source.readAll(), source.positionCodec());
     }
 
     EventReader toReader() {
