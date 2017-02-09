45842fcdf486ce7dfe2610b4c6c6724b22c7f2a6
Tom Denley <tom.denley@timgroup.com>
2017-02-09 15:24:42 +0000

    tomd: serialise the position as a json string, keyed by the stream name

--- stitching/src/main/java/com/timgroup/eventstore/merging/MergedEventReaderPosition.java
+++ stitching/src/main/java/com/timgroup/eventstore/merging/MergedEventReaderPosition.java
@@ -1,9 +1,16 @@
 package com.timgroup.eventstore.merging;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.timgroup.eventstore.api.Position;
 import com.timgroup.eventstore.api.PositionCodec;
 
-import java.util.regex.Pattern;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.Long.parseLong;
 
 final class MergedEventReaderPosition implements Position {
     final long outputEventNumber;
@@ -21,8 +28,9 @@ final class MergedEventReaderPosition implements Position {
     }
 
     static final class MergedEventReaderPositionCodec implements PositionCodec {
-        private static final String SEPARATOR = "###";
-        private static final Pattern SEPARATOR_REGEX = Pattern.compile(Pattern.quote(SEPARATOR));
+        private static final String EVENT_NUMBER_FIELD = "eventnumber";
+
+        private final ObjectMapper objectMapper = new ObjectMapper();
 
         private final NamedReaderWithCodec[] namedReaders;
 
@@ -33,29 +41,36 @@ final class MergedEventReaderPosition implements Position {
         @Override
         public String serializePosition(Position position) {
             MergedEventReaderPosition mergedPosition = (MergedEventReaderPosition)position;
-            StringBuilder serialisedPositionData = new StringBuilder();
 
-            serialisedPositionData
-                    .append(mergedPosition.outputEventNumber)
-                    .append(SEPARATOR);
-            //TODO: serialize out the stream name rather than relying on its index
+            Map<String, String> positionMap = new HashMap<>();
+
+            positionMap.put(EVENT_NUMBER_FIELD, String.valueOf(mergedPosition.outputEventNumber));
             for (int index = 0; index < namedReaders.length; index++) {
-                serialisedPositionData
-                        .append(namedReaders[index].codec.serializePosition(mergedPosition.inputPositions[index]))
-                        .append(SEPARATOR);
+                positionMap.put(namedReaders[index].name, namedReaders[index].codec.serializePosition(mergedPosition.inputPositions[index]));
             }
-            return serialisedPositionData.toString();
+
+            try {
+                return objectMapper.writeValueAsString(positionMap);
+            } catch (JsonProcessingException e) {
+                throw new IllegalArgumentException("unable to serialise position", e);
+            }
         }
 
         @Override
         public Position deserializePosition(String serialisedPosition) {
-            String[] serialisedPositionData = SEPARATOR_REGEX.split(serialisedPosition);
+            try {
+                JsonNode positionMap = objectMapper.readTree(serialisedPosition);
 
-            Position[] deserialisedPositions = new Position[namedReaders.length];
-            for (int index = 0; index < namedReaders.length; index++) {
-                deserialisedPositions[index] = namedReaders[index].codec.deserializePosition(serialisedPositionData[index + 1]);
+                Position[] deserialisedPositions = new Position[namedReaders.length];
+                for (int index = 0; index < namedReaders.length; index++) {
+                    deserialisedPositions[index] = namedReaders[index].codec.deserializePosition(positionMap.get(namedReaders[index].name).asText());
+                }
+
+                return new MergedEventReaderPosition(parseLong(positionMap.get(EVENT_NUMBER_FIELD).asText()), deserialisedPositions);
+
+            } catch (IOException e) {
+                throw new IllegalArgumentException("unable to deserialise position", e);
             }
-            return new MergedEventReaderPosition(Long.parseLong(serialisedPositionData[0]), deserialisedPositions);
         }
     }
 
