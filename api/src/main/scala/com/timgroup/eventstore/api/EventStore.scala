0c8f9d490a1663132a79f727214bb18cdb3e3ccb
Tom Denley <tom.denley@timgroup.com>
2017-02-16 17:32:05 +0000

    tomd: deprecate moar things

--- api/src/main/scala/com/timgroup/eventstore/api/EventStore.scala
+++ api/src/main/scala/com/timgroup/eventstore/api/EventStore.scala
@@ -17,8 +17,13 @@ trait EventStore {
 /**
   * @deprecated uaw EventSource instead
   */
+@Deprecated
 trait EventStream extends Iterator[EventInStream]
 
+/**
+  * @deprecated uaw EventSource instead
+  */
+@Deprecated
 case class Body(data: Array[Byte]) {
   override def equals(obj: scala.Any): Boolean = {
     if (obj != null && obj.getClass == getClass) {
@@ -29,18 +34,33 @@ case class Body(data: Array[Byte]) {
   }
 }
 
+/**
+  * @deprecated uaw EventSource instead
+  */
+@Deprecated
 case class EventData(eventType: String, body: Body)
 
+/**
+  * @deprecated uaw EventSource instead
+  */
 object EventData {
   def apply(eventType: String, body: Array[Byte]): EventData = EventData(eventType, Body(body))
 }
 
+/**
+  * @deprecated uaw EventSource instead
+  */
+@Deprecated
 case class EventInStream(effectiveTimestamp: DateTime,
                          eventData: EventData,
                          version: Long) {
   val position = LegacyPositionAdapter(version)
 }
 
+/**
+  * @deprecated uaw EventSource instead
+  */
+@Deprecated
 case class LegacyPositionAdapter(version: Long) extends Position {
   override def toString: String = version.toString
 }
