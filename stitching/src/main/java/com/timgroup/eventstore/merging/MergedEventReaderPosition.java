59ee404cf7a032e7edeae4f0899f8975a165335b
Steve Haslam <steve.haslam@timgroup.com>
2017-03-28 16:17:38 +0100

    SteveH: only use jackson-core in merged event reader codec

--- stitching/src/main/java/com/timgroup/eventstore/merging/MergedEventReaderPosition.java
+++ stitching/src/main/java/com/timgroup/eventstore/merging/MergedEventReaderPosition.java
@@ -1,14 +1,14 @@
 package com.timgroup.eventstore.merging;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.io.StringWriter;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 import com.timgroup.eventstore.api.Position;
 import com.timgroup.eventstore.api.PositionCodec;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 final class MergedEventReaderPosition implements Position {
     private final String[] names;
@@ -36,8 +36,7 @@ final class MergedEventReaderPosition implements Position {
     }
 
     static final class MergedEventReaderPositionCodec implements PositionCodec {
-        private final ObjectMapper objectMapper = new ObjectMapper();
-
+        private final JsonFactory jsonFactory = new JsonFactory();
         private final NamedReaderWithCodec[] namedReaders;
 
         MergedEventReaderPositionCodec(NamedReaderWithCodec[] namedReaders) {
@@ -48,40 +47,62 @@ final class MergedEventReaderPosition implements Position {
         public String serializePosition(Position position) {
             MergedEventReaderPosition mergedPosition = (MergedEventReaderPosition) position;
 
-            Map<String, String> positionMap = new HashMap<>();
-
-            for (int index = 0; index < namedReaders.length; index++) {
-                positionMap.put(namedReaders[index].name, namedReaders[index].codec.serializePosition(mergedPosition.inputPositions[index]));
+            StringWriter stringWriter = new StringWriter();
+            try (JsonGenerator jgen = jsonFactory.createGenerator(stringWriter)) {
+                jgen.writeStartObject();
+                for (int index = 0; index < namedReaders.length; index++) {
+                    jgen.writeStringField(namedReaders[index].name, namedReaders[index].codec.serializePosition(mergedPosition.inputPositions[index]));
+                }
+                jgen.writeEndObject();
+            } catch (IOException e) {
+                throw new RuntimeException("unable to serialise position", e);
             }
-
-            try {
-                return objectMapper.writeValueAsString(positionMap);
-            } catch (JsonProcessingException e) {
-                throw new IllegalArgumentException("unable to serialise position", e);
-            }
+            return stringWriter.toString();
         }
 
         @Override
         public Position deserializePosition(String serialisedPosition) {
-            try {
-                JsonNode positionMap = objectMapper.readTree(serialisedPosition);
-                if (positionMap.size() > namedReaders.length) {
-                    throw new IllegalArgumentException("Bad position, containing unexpected keys " + serialisedPosition);
+            try (JsonParser jpars = jsonFactory.createParser(serialisedPosition)) {
+                if (jpars.nextToken() != JsonToken.START_OBJECT) {
+                    throw new IllegalArgumentException("Bad position, expected JSON object, found " + jpars.getCurrentToken() + ": " + serialisedPosition);
                 }
 
                 String[] names = new String[namedReaders.length];
                 Position[] deserialisedPositions = new Position[namedReaders.length];
-                for (int index = 0; index < namedReaders.length; index++) {
-                    String name = namedReaders[index].name;
-                    names[index] = name;
-                    if (!positionMap.has(name)) {
-                        throw new IllegalArgumentException("Bad position, containing no key for " + name + " :" + serialisedPosition);
+                while (jpars.nextToken() == JsonToken.FIELD_NAME) {
+                    String name = jpars.getCurrentName();
+                    int readerIndex = -1;
+                    for (int index = 0; index < namedReaders.length; index++) {
+                        if (namedReaders[index].name.equals(name)) {
+                            readerIndex = index;
+                            break;
+                        }
                     }
-                    deserialisedPositions[index] = namedReaders[index].codec.deserializePosition(positionMap.get(name).asText());
+                    if (readerIndex < 0) {
+                        throw new IllegalArgumentException("Bad position, containing unexpected keys " + serialisedPosition);
+                    }
+                    if (deserialisedPositions[readerIndex] != null) {
+                        throw new IllegalArgumentException("Bad position, containing duplicated keys " + serialisedPosition);
+                    }
+                    if (jpars.nextToken() != JsonToken.VALUE_STRING) {
+                        throw new IllegalArgumentException("Expected JSON string for field \"" + name + "\", found " + jpars.getCurrentToken() + ": " + serialisedPosition);
+                    }
+                    String valueString = jpars.getText();
+                    deserialisedPositions[readerIndex] = namedReaders[readerIndex].codec.deserializePosition(valueString);
+                    names[readerIndex] = name;
+                }
+
+                if (jpars.getCurrentToken() != JsonToken.END_OBJECT) {
+                    throw new IllegalArgumentException("Bad position, expected end of JSON object, found " + jpars.getCurrentToken() + ": " + serialisedPosition);
+                }
+
+                for (int i = 0; i < namedReaders.length; i++) {
+                    if (deserialisedPositions[i] == null) {
+                        throw new IllegalArgumentException("Bad position, containing no key for " + namedReaders[i].name + " :" + serialisedPosition);
+                    }
                 }
 
                 return new MergedEventReaderPosition(names, deserialisedPositions);
-
             } catch (IOException e) {
                 throw new IllegalArgumentException("unable to deserialise position", e);
             }
