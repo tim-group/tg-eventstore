874ea21af834f8b26b180d1bbed6e469a5ccb140
Steve Haslam <steve.haslam@timgroup.com>
2017-05-18 14:43:59 +0100

    SteveH: common way to concatenate multiple event handlers into one

--- subscription/src/main/java/com/timgroup/eventsubscription/EventHandler.java
+++ subscription/src/main/java/com/timgroup/eventsubscription/EventHandler.java
@@ -9,4 +9,23 @@ public interface EventHandler<T> {
     default void apply(Position position, DateTime timestamp, T deserialized, boolean endOfBatch) {
         apply(deserialized);
     }
+
+    @SafeVarargs
+    static <E> EventHandler<E> concat(EventHandler<E>... handlers) {
+        return new EventHandler<E>() {
+            @Override
+            public void apply(E deserialized) {
+                for (EventHandler<E> handler : handlers) {
+                    handler.apply(deserialized);
+                }
+            }
+
+            @Override
+            public void apply(Position position, DateTime timestamp, E deserialized, boolean endOfBatch) {
+                for (EventHandler<E> handler : handlers) {
+                    handler.apply(position, timestamp, deserialized, endOfBatch);
+                }
+            }
+        };
+    }
 }
