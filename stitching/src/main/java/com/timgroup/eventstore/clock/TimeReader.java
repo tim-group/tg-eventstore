1b2e8fe0d33a0f85d30ad5a370d107a65bbf1802
Steve Haslam <steve.haslam@timgroup.com>
2017-05-31 14:43:33 +0100

    SteveH/JHeister: add event reader to produce clock tick event stream
    Intended for merging into other events to interlave them with clock
    ticks (TTLs etc)

---
+++ stitching/src/main/java/com/timgroup/eventstore/clock/TimeReader.java
@@ -0,0 +1,102 @@
+package com.timgroup.eventstore.clock;
+
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+import com.timgroup.eventstore.api.EventReader;
+import com.timgroup.eventstore.api.Position;
+import com.timgroup.eventstore.api.PositionCodec;
+import com.timgroup.eventstore.api.ResolvedEvent;
+import com.timgroup.eventstore.api.StreamId;
+import com.timgroup.eventstore.merging.NamedReaderWithCodec;
+
+import static com.timgroup.eventstore.api.EventRecord.eventRecord;
+
+public class TimeReader implements EventReader, PositionCodec {
+    private final Instant start;
+    private final Duration accuracy;
+    private final Clock clock;
+
+    public TimeReader(Instant start, Duration accuracy, Clock clock) {
+        this.start = start;
+        this.accuracy = accuracy;
+        this.clock = clock;
+    }
+
+    public static NamedReaderWithCodec timePassedEventStream(Instant start, Duration accuracy, Clock clock) {
+        TimeReader timeReader = new TimeReader(start, accuracy, clock);
+        return new NamedReaderWithCodec("Clock", timeReader, timeReader);
+    }
+
+    @Override
+    public Stream<ResolvedEvent> readAllForwards(Position positionExclusive) {
+        long now = clock.millis();
+        long lastTime = ((TimePosition) positionExclusive).value;
+
+        long numberOfEvents = (now - lastTime) / accuracy.toMillis();
+
+        return LongStream.range(0, numberOfEvents).map(operand -> lastTime + ((operand + 1) * accuracy.toMillis())).mapToObj(time -> new ResolvedEvent(
+                new TimePosition(time),
+                eventRecord(
+                        Instant.ofEpochMilli(time),
+                        StreamId.streamId("time", ""),
+                        -1,
+                        "TimePassed",
+                        new byte[0],
+                        new byte[0]
+                )));
+    }
+
+    @Override
+    public Position emptyStorePosition() {
+        return new TimePosition(start.toEpochMilli());
+    }
+
+    @Override
+    public Position deserializePosition(String string) {
+        return new TimePosition(Long.parseLong(string));
+    }
+
+    @Override
+    public String serializePosition(Position position) {
+        return Long.toString(((TimePosition) position).value);
+    }
+
+    private static final class TimePosition implements Position, Comparable<TimePosition> {
+        private final long value;
+
+        private TimePosition(long value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "TimePosition{" +
+                    "value=" + value +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            TimePosition that = (TimePosition) o;
+
+            return value == that.value;
+        }
+
+        @Override
+        public int hashCode() {
+            return (int) (value ^ (value >>> 32));
+        }
+
+        @Override
+        public int compareTo(TimePosition o) {
+            return Long.compare(value, o.value);
+        }
+    }
+}
---
+++ stitching/src/test/java/com/timgroup/eventstore/clock/TimeReaderTest.java
@@ -0,0 +1,37 @@
+package com.timgroup.eventstore.clock;
+
+import java.time.Duration;
+import java.time.Instant;
+
+import com.timgroup.clocks.testing.ManualClock;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class TimeReaderTest {
+    private final ManualClock clock = ManualClock.createDefault();
+    private final Instant start = clock.instant();
+    private final TimeReader reader = new TimeReader(clock.instant(), Duration.ofSeconds(10), clock);
+
+    @Test
+    public void
+    when_less_time_than_accuracy_has_passed_no_events() {
+        assertThat(reader.readAllForwards().count(), is(0L));
+        clock.bump(Duration.ofMillis(9999));
+        assertThat(reader.readAllForwards().count(), is(0L));
+    }
+
+    @Test
+    public void
+    when_enough_time_has_passed_events() {
+        clock.bump(Duration.ofMillis(39999));
+        assertThat(reader.readAllForwards().map(r -> r.eventRecord().timestamp()).collect(toList()), Matchers.contains(
+                start.plusMillis(10000),
+                start.plusMillis(20000),
+                start.plusMillis(30000)
+        ));
+    }
+}
