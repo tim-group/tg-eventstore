70daa4a5d3ec28907490c3bb861019a57d2ea9df
Steve Haslam <steve.haslam@timgroup.com>
2017-08-11 16:21:34 +0100

    SteveH: include factory methods for Deserializer
    Having the 'filtering' method in here helps a bit to remind me why there
    is this mess with two methods in Deserializer at all

--- subscription/src/main/java/com/timgroup/eventsubscription/Deserializer.java
+++ subscription/src/main/java/com/timgroup/eventsubscription/Deserializer.java
@@ -1,6 +1,8 @@
 package com.timgroup.eventsubscription;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 import com.timgroup.eventstore.api.EventRecord;
 
@@ -11,4 +13,23 @@ public interface Deserializer<T> {
         consumer.accept(deserialize(event));
     }
 
+    static <T> Deserializer<T> applying(Function<? super EventRecord, ? extends T> function) {
+        return function::apply;
+    }
+
+    static <T> Deserializer<T> filtering(Predicate<? super EventRecord> predicate, Deserializer<? extends T> downstream) {
+        return new Deserializer<T>() {
+            @Override
+            public void deserialize(EventRecord event, Consumer<T> consumer) {
+                if (predicate.test(event)) {
+                    consumer.accept(downstream.deserialize(event));
+                }
+            }
+
+            @Override
+            public T deserialize(EventRecord event) {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
 }
