6300bb323c7837c44834b0fb5434274b39157de4
Edward Wong <edward.yueshungwong@timgroup.com>
2017-11-15 21:49:35 +0000

    Edward: refactored the test to remove duplication

--- datastream/src/test/java/com/timgroup/eventstore/datastream/CachingEventReaderTest.java
+++ datastream/src/test/java/com/timgroup/eventstore/datastream/CachingEventReaderTest.java
@@ -1,10 +1,10 @@
 package com.timgroup.eventstore.datastream;
 
+import com.timgroup.eventstore.api.EventReader;
 import com.timgroup.eventstore.api.NewEvent;
 import com.timgroup.eventstore.api.ResolvedEvent;
 import com.timgroup.eventstore.api.StreamId;
 import com.timgroup.eventstore.memory.JavaInMemoryEventStore;
-import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -13,18 +13,21 @@ import org.junit.rules.TemporaryFolder;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.time.Clock;
-import java.util.UUID;
-import java.util.stream.Collectors;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static com.timgroup.eventstore.api.NewEvent.newEvent;
 import static com.timgroup.eventstore.api.StreamId.streamId;
+import static com.timgroup.eventstore.memory.JavaInMemoryEventStore.CODEC;
 import static java.nio.charset.StandardCharsets.UTF_8;
-import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+import static java.util.UUID.randomUUID;
 import static java.util.stream.Collectors.toList;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 
 public class CachingEventReaderTest {
     @Rule
@@ -35,77 +38,71 @@ public class CachingEventReaderTest {
 
     JavaInMemoryEventStore underlyingEventStore = new JavaInMemoryEventStore(Clock.systemUTC());
 
-    private final String category_1 = randomCategory();
-
-    private final StreamId stream_1 = streamId(category_1, "1");
-
+    private final StreamId stream_1 = streamId(randomCategory(), "1");
     private final NewEvent event_1 = anEvent();
 
     @Before public void init() throws IOException {
         temporaryFolder.create();
         cacheDirectory = temporaryFolder.getRoot().toPath();
-        cachingEventReader = new CachingEventReader(underlyingEventStore, JavaInMemoryEventStore.CODEC, cacheDirectory);
+        cachingEventReader = new CachingEventReader(underlyingEventStore, CODEC, cacheDirectory);
+    }
+
+    private List<ResolvedEvent> readAllToList(EventReader eventReader) {
+        return eventReader.readAllForwards().collect(toList());
     }
 
     @Test
     public void
     givenNoDataInUnderlying_returnsNothing() {
-        assertThat(cachingEventReader.readAllForwards().collect(Collectors.toList()), Matchers.empty());
+        assertThat(readAllToList(cachingEventReader), is(empty()));
     }
 
     @Test
     public void
     givenAnEventDataInUnderlying_returnsThatEvent() {
-        underlyingEventStore.write(stream_1, asList(event_1));
+        underlyingEventStore.write(stream_1, singletonList(event_1));
 
-        assertThat(cachingEventReader.readAllForwards().collect(toList()), hasSize(1));
-
-        assertThat(cachingEventReader.readAllForwards().collect(toList()),
-                equalTo(underlyingEventStore.readAllForwards().collect(toList())));
+        assertThat(readAllToList(cachingEventReader), hasSize(1));
+        assertThat(readAllToList(cachingEventReader), equalTo(underlyingEventStore.readAllForwards().collect(toList())));
     }
 
     @Test
     public void
     givenAlreadyReadAllOnce_readsFromTheCache() {
-        underlyingEventStore.write(stream_1, asList(event_1));
+        underlyingEventStore.write(stream_1, singletonList(event_1));
 
         Stream<ResolvedEvent> resolvedEventStream = cachingEventReader.readAllForwards();
         assertThat(resolvedEventStream.collect(toList()), hasSize(1));
 
-        CachingEventReader newCachingEventReader = new CachingEventReader(new JavaInMemoryEventStore(Clock.systemUTC()),  JavaInMemoryEventStore.CODEC, cacheDirectory);
+        CachingEventReader newCachingEventReader = new CachingEventReader(new JavaInMemoryEventStore(Clock.systemUTC()),  CODEC, cacheDirectory);
+        assertThat(readAllToList(newCachingEventReader), hasSize(1));
+        assertThat(readAllToList(newCachingEventReader), equalTo(underlyingEventStore.readAllForwards().collect(toList())));
+    }
 
-        assertThat(newCachingEventReader.readAllForwards().collect(toList()), hasSize(1));
-
-        assertThat(newCachingEventReader.readAllForwards().collect(toList()),
-                equalTo(underlyingEventStore.readAllForwards().collect(toList())));
+    private List<ResolvedEvent> readAllFromEmpty(EventReader eventReader) {
+        return eventReader.readAllForwards(underlyingEventStore.emptyStorePosition()).collect(toList());
     }
 
     @Test
     public void
     givenReadingFromEmptyStorePosition_readsFromTheCache() {
-        underlyingEventStore.write(stream_1, asList(event_1));
+        underlyingEventStore.write(stream_1, singletonList(event_1));
+        assertThat(readAllFromEmpty(cachingEventReader), hasSize(1));
 
-        Stream<ResolvedEvent> resolvedEventStream = cachingEventReader.readAllForwards(underlyingEventStore.emptyStorePosition());
-        assertThat(resolvedEventStream.collect(toList()), hasSize(1));
-
-        CachingEventReader newCachingEventReader = new CachingEventReader(new JavaInMemoryEventStore(Clock.systemUTC()),  JavaInMemoryEventStore.CODEC, cacheDirectory);
-
-        assertThat(newCachingEventReader.readAllForwards(underlyingEventStore.emptyStorePosition()).collect(toList()), hasSize(1));
-
-        assertThat(newCachingEventReader.readAllForwards(underlyingEventStore.emptyStorePosition()).collect(toList()),
-                equalTo(underlyingEventStore.readAllForwards().collect(toList())));
+        CachingEventReader newCachingEventReader = new CachingEventReader(new JavaInMemoryEventStore(Clock.systemUTC()),  CODEC, cacheDirectory);
+        assertThat(readAllFromEmpty(newCachingEventReader), hasSize(1));
+        assertThat(readAllFromEmpty(newCachingEventReader), equalTo(underlyingEventStore.readAllForwards().collect(toList())));
     }
 
-
     private static NewEvent anEvent() {
-        return newEvent(UUID.randomUUID().toString(), randomData(), randomData());
+        return newEvent(randomUUID().toString(), randomData(), randomData());
     }
 
     private static String randomCategory() {
-        return "stream_" + UUID.randomUUID().toString().replace("-", "");
+        return "stream_" + randomUUID().toString().replace("-", "");
     }
 
     private static byte[] randomData() {
-        return ("{\n  \"value\": \"" + UUID.randomUUID() + "\"\n}").getBytes(UTF_8);
+        return ("{\n  \"value\": \"" + randomUUID() + "\"\n}").getBytes(UTF_8);
     }
 }
