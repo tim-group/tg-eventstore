fa45c79bac458008062c8cb1b30a9e8fb4402231
Brian Roberts <brian.roberts@timgroup.com>
2017-11-15 14:52:20 +0000

    Brian/Edward: first basic implementation of the DataStreamEventReader * This doesn't handle exceptions * Only works on one cache file

--- datastream/src/main/java/com/timgroup/eventstore/datastream/DataStreamEventReader.java
+++ datastream/src/main/java/com/timgroup/eventstore/datastream/DataStreamEventReader.java
@@ -1,21 +1,154 @@
 package com.timgroup.eventstore.datastream;
 
-import com.timgroup.eventstore.api.EventReader;
-import com.timgroup.eventstore.api.Position;
-import com.timgroup.eventstore.api.ResolvedEvent;
+import com.timgroup.eventstore.api.*;
 
+import java.io.*;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Instant;
+import java.util.Optional;
+import java.util.Spliterator;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import static java.lang.Long.MAX_VALUE;
+import static java.util.stream.StreamSupport.stream;
 
 public class DataStreamEventReader implements EventReader {
     private final EventReader underlying;
+    private final PositionCodec positionCodec;
+    private final Path cacheDirectory;
 
-    public DataStreamEventReader(EventReader underlying) {
+    private static String cacheFileName = "cache";
+
+    public DataStreamEventReader(EventReader underlying, PositionCodec positionCodec, Path cacheDirectory) {
         this.underlying = underlying;
+        this.positionCodec = positionCodec;
+        this.cacheDirectory = cacheDirectory;
     }
 
     @Override
     public Stream<ResolvedEvent> readAllForwards() {
-        return underlying.readAllForwards();
+        Stream<ResolvedEvent> cachedEvents = readAllCachedEvents();
+        try {
+            File tmpOutputFile = File.createTempFile("cache", "inprogess", cacheDirectory.toFile());
+            DataOutputStream output = new DataOutputStream(new FileOutputStream(tmpOutputFile));
+            return Stream.concat(cachedEvents, underlying.readAllForwards().peek(resolvedEvent -> writeEvent(output, resolvedEvent)).onClose(() -> {
+                try {
+                    output.close();
+                    tmpOutputFile.renameTo(cacheDirectory.resolve(cacheFileName).toFile()); // todo don't ignore rename
+                } catch (IOException e) {
+                    e.printStackTrace(); // todo
+                }
+            }));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace(); // todo
+            throw new RuntimeException("Error reading cache", e);
+        } catch (IOException e) {
+            e.printStackTrace(); // todo
+            throw new RuntimeException("Error reading cache", e);
+        }
+    }
+
+    private void writeEvent(DataOutputStream output, ResolvedEvent resolvedEvent) {
+        try {
+            output.writeUTF(positionCodec.serializePosition(resolvedEvent.position()));
+            EventRecord eventRecord = resolvedEvent.eventRecord();
+            output.writeLong(eventRecord.timestamp().toEpochMilli());
+            StreamId streamId = eventRecord.streamId();
+            output.writeUTF(streamId.category());
+            output.writeUTF(streamId.id());
+            output.writeLong(eventRecord.eventNumber());
+            output.writeUTF(eventRecord.eventType());
+            output.writeInt(eventRecord.data().length);
+            output.write(eventRecord.data());
+            output.writeInt(eventRecord.metadata().length);
+            output.write(eventRecord.metadata());
+        } catch (IOException e) {
+            e.printStackTrace(); // todo
+        }
+    }
+
+    private Stream<ResolvedEvent> readAllCachedEvents() {
+        Path cachePath = cacheDirectory.resolve(cacheFileName);
+        File cacheFile = cachePath.toFile();
+        Optional<Path> cachedFiles = cacheFile.exists() ? Optional.of(cacheFile.toPath()) : Optional.empty();
+        Spliterator<ResolvedEvent> spliterator = new Spliterator<ResolvedEvent>() {
+            private DataInputStream current = null;
+
+            @Override
+            public boolean tryAdvance(Consumer<? super ResolvedEvent> action) {
+                if (current == null && cachedFiles.isPresent()) {
+                    cachedFiles.ifPresent(path -> {
+                        try {
+                            current = new DataInputStream(new FileInputStream(path.toFile()));
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace(); // todo
+                        }
+                    });
+                }
+                if (current != null) {
+                    return readNextEvent(action);
+                } else {
+                    return false;
+                }
+            }
+
+            private boolean readNextEvent(Consumer<? super ResolvedEvent> action) {
+                try {
+                    ResolvedEvent resolvedEvent = new ResolvedEvent(positionCodec.deserializePosition(current.readUTF()),
+                            EventRecord.eventRecord(Instant.ofEpochMilli(current.readLong()),
+                                    StreamId.streamId(current.readUTF(), current.readUTF()),
+                                    current.readLong(),
+                                    current.readUTF(),
+                                    readByteArray(current),
+                                    readByteArray(current)));
+                    action.accept(resolvedEvent);
+                    return true;
+                } catch (EOFException e) {
+                    // todo: this should only be OK if throw from first read (position reading)
+                    return false;
+                } catch (IOException e) {
+                    e.printStackTrace(); // todo
+                    return false;
+                }
+            }
+
+            private byte[] readByteArray(DataInputStream current) throws IOException {
+                int size = current.readInt();
+                byte[] buffer = new byte[size];
+                current.readFully(buffer);
+                return buffer;
+            }
+
+            @Override
+            public Spliterator<ResolvedEvent> trySplit() {
+                return null;
+            }
+
+            @Override
+            public long estimateSize() {
+                return MAX_VALUE;
+            }
+
+            @Override
+            public int characteristics() {
+                return ORDERED | NONNULL | DISTINCT;
+            }
+
+        };
+        return stream(spliterator, false);
+    }
+
+    private Stream<Path> findAllCachedFiles() {
+        try {
+            return Files.find(cacheDirectory, 1, (file, attributes) -> true);
+        } catch (IOException e) {
+            e.printStackTrace(); // todo
+            throw new RuntimeException("Error finding cache files", e);
+        }
     }
 
     @Override
--- datastream/src/test/java/com/timgroup/eventstore/datastream/DataStreamEventReaderTest.java
+++ datastream/src/test/java/com/timgroup/eventstore/datastream/DataStreamEventReaderTest.java
@@ -3,15 +3,19 @@ package com.timgroup.eventstore.datastream;
 import com.timgroup.eventstore.api.*;
 import com.timgroup.eventstore.memory.JavaInMemoryEventStore;
 import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
+import java.io.IOException;
+import java.nio.file.Path;
 import java.time.Clock;
-import java.util.Arrays;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.timgroup.eventstore.api.NewEvent.newEvent;
-import static com.timgroup.eventstore.api.ObjectPropertiesMatcher.objectWith;
 import static com.timgroup.eventstore.api.StreamId.streamId;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Arrays.asList;
@@ -22,15 +26,29 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 
 public class DataStreamEventReaderTest {
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+    Path cacheDirectory;
+
+    DataStreamEventReader dataStreamEventReader;
+
     JavaInMemoryEventStore eventStore = new JavaInMemoryEventStore(Clock.systemUTC());
     EventReader underlying = eventStore;
-    DataStreamEventReader dataStreamEventReader = new DataStreamEventReader(underlying);
 
     private final String category_1 = randomCategory();
 
     private final StreamId stream_1 = streamId(category_1, "1");
 
     private final NewEvent event_1 = anEvent();
+
+    public DataStreamEventReaderTest() {
+    }
+
+    @Before public void init() throws IOException {
+        temporaryFolder.create();
+        cacheDirectory = temporaryFolder.getRoot().toPath();
+        dataStreamEventReader = new DataStreamEventReader(underlying, JavaInMemoryEventStore.CODEC, cacheDirectory);
+    }
 
     @Test
     public void
@@ -46,6 +64,23 @@ public class DataStreamEventReaderTest {
         assertThat(dataStreamEventReader.readAllForwards().collect(toList()), hasSize(1));
 
         assertThat(dataStreamEventReader.readAllForwards().collect(toList()),
+                equalTo(eventStore.readAllForwards().collect(toList())));
+    }
+
+    @Test
+    public void
+    givenAlreadyReadAllOnce_readsFromTheCache() {
+        eventStore.write(stream_1, asList(event_1));
+
+        Stream<ResolvedEvent> resolvedEventStream = dataStreamEventReader.readAllForwards();
+        assertThat(resolvedEventStream.collect(toList()), hasSize(1));
+        resolvedEventStream.close();
+
+        DataStreamEventReader newDataStreamEventReader = new DataStreamEventReader(new JavaInMemoryEventStore(Clock.systemUTC()),  JavaInMemoryEventStore.CODEC, cacheDirectory);
+
+        assertThat(newDataStreamEventReader.readAllForwards().collect(toList()), hasSize(1));
+
+        assertThat(newDataStreamEventReader.readAllForwards().collect(toList()),
                 equalTo(eventStore.readAllForwards().collect(toList())));
     }
 
