d26add1fd79c1d8fca7dcb841fde4358cb6b44d0
Alex Conlin-Oakley <alex.conlin-oakley@acuris.com>
2017-11-27 17:24:11 +0000

    Alex/Brian: Add table name to status page component label

--- mysql/src/main/java/com/timgroup/eventstore/mysql/BasicMysqlEventSource.java
+++ mysql/src/main/java/com/timgroup/eventstore/mysql/BasicMysqlEventSource.java
@@ -64,7 +64,7 @@ public class BasicMysqlEventSource implements EventSource {
     @Override
     public Collection<Component> monitoring() {
         String id = "EventStore-" + this.name;
-        String label = "EventStore (" + this.name + ")";
+        String label = "EventStore (name: " + this.name + ", table: " + this.tableName +")";
         return singletonList(new DatabaseConnectionComponent(id, label, connectionProvider::getConnection));
     }
 
