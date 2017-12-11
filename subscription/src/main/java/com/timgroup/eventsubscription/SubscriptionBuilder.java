17a2317e61a2cf8b99850dc782509e21bf838315
Steve Haslam <steve.haslam@timgroup.com>
2017-12-11 16:12:28 +0000

    SteveH: remove type-unsafe vararg publishingTo variant
    Support calling publishingTo multiple times to add targets individually,
    although the methods to accept a list are still there.
    
    Similarly allow multiple calls to withListeners/withListener to add
    to set of listeners

--- subscription/src/main/java/com/timgroup/eventsubscription/SubscriptionBuilder.java
+++ subscription/src/main/java/com/timgroup/eventsubscription/SubscriptionBuilder.java
@@ -1,15 +1,5 @@
 package com.timgroup.eventsubscription;
 
-import java.time.Clock;
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Stream;
-
 import com.timgroup.eventstore.api.EventCategoryReader;
 import com.timgroup.eventstore.api.EventReader;
 import com.timgroup.eventstore.api.Position;
@@ -17,6 +7,16 @@ import com.timgroup.eventstore.api.ResolvedEvent;
 import com.timgroup.eventsubscription.healthcheck.SubscriptionListener;
 import com.timgroup.structuredevents.EventSink;
 import com.timgroup.structuredevents.Slf4jEventSink;
+
+import java.time.Clock;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 import static java.util.Objects.requireNonNull;
 
@@ -26,8 +26,8 @@ public class SubscriptionBuilder<T> {
     private Duration runFrequency = Duration.ofSeconds(1);
     private Duration maxInitialReplayDuration = Duration.ofSeconds(1);
     private int bufferSize = 1024;
-    private List<EventHandler<? super T>> handlers = Collections.emptyList();
-    private List<SubscriptionListener> listeners = Collections.emptyList();
+    private final List<EventHandler<? super T>> handlers = new ArrayList<>();
+    private final List<SubscriptionListener> listeners = new ArrayList<>();
 
     private Function<Position, Stream<ResolvedEvent>> reader = null;
     private Position startingPosition = null;
@@ -90,26 +90,35 @@ public class SubscriptionBuilder<T> {
         return this;
     }
 
-    public SubscriptionBuilder<T> publishingTo(EventHandler<T>... handlers) {
-        this.handlers = Arrays.asList(handlers);
+    public SubscriptionBuilder<T> publishingTo(Collection<EventHandler<T>> handlers) {
+        this.handlers.addAll(handlers);
         return this;
     }
 
-    public SubscriptionBuilder<T> publishingTo(Collection<EventHandler<T>> handlers) {
-        this.handlers = new ArrayList<>(handlers);
+    public SubscriptionBuilder<T> publishingTo(EventHandler<? super T> handler) {
+        this.handlers.add(handler);
+        return this;
+    }
+
+    public SubscriptionBuilder<T> publishingTo(Consumer<? super T> handler) {
+        this.handlers.add(EventHandler.ofConsumer(handler));
         return this;
     }
 
     public SubscriptionBuilder<T> withListeners(SubscriptionListener... listeners) {
-        this.listeners = Arrays.asList(listeners);
+        this.listeners.addAll(Arrays.asList(listeners));
         return this;
     }
 
     public SubscriptionBuilder<T> withListeners(Collection<SubscriptionListener> listeners) {
-        this.listeners = new ArrayList<>(listeners);
+        this.listeners.addAll(listeners);
         return this;
     }
 
+    public SubscriptionBuilder<T> withListener(SubscriptionListener listener) {
+        this.listeners.add(listener);
+        return this;
+    }
 
     public SubscriptionBuilder<T> withEventSink(EventSink eventSink) {
         this.eventSink = eventSink;
