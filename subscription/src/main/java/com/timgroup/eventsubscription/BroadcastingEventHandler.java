53f0e4f0e6b5e08a33563f249ce787f722167f47
Steve Haslam <steve.haslam@timgroup.com>
2018-11-23 17:56:32 +0000

    SteveH: unmark BroadcastingEventHandler as public and have it propagate both method calls

--- subscription/src/main/java/com/timgroup/eventsubscription/BroadcastingEventHandler.java
+++ subscription/src/main/java/com/timgroup/eventsubscription/BroadcastingEventHandler.java
@@ -3,20 +3,27 @@ package com.timgroup.eventsubscription;
 import com.timgroup.eventstore.api.Position;
 import org.joda.time.DateTime;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
-public class BroadcastingEventHandler<T> implements EventHandler<T> {
+final class BroadcastingEventHandler<T> implements EventHandler<T> {
     private final List<EventHandler<? super T>> handlers;
 
-    public BroadcastingEventHandler(List<EventHandler<? super T>> handlers) {
-        this.handlers = new CopyOnWriteArrayList<>(handlers);
+    BroadcastingEventHandler(List<EventHandler<? super T>> handlers) {
+        this.handlers = new ArrayList<>(handlers);
     }
 
     @Override
     public void apply(Position position, DateTime timestamp, T deserialized, boolean endOfBatch) {
         for (EventHandler<? super T> handler : handlers) {
             handler.apply(position, timestamp, deserialized, endOfBatch);
+        }
+    }
+
+    @Override
+    public void apply(T deserialized) {
+        for (EventHandler<? super T> handler : handlers) {
+            handler.apply(deserialized);
         }
     }
 }
