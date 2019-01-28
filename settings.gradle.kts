f6cbf596aa3ed9abbc116119e3f8cb4d466588e1
Steve Haslam <steve.haslam@timgroup.com>
2019-01-28 19:32:01 +0000

    SteveH: Gradle 5.1

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        val repoUrl: String by settings
+        maven(url = "$repoUrl/groups/public")
+    }
+}
+
+rootProject.name = "tg-eventstore"
+
+include("api")
+include("api:testlib")
+include("api-legacy")
+include("api-legacy:testlib")
+include("diffing")
+include("memory")
+include("memory-legacy")
+include("mysql")
+include("mysql-legacy")
+include("filesystem")
+include("stitching")
+include("stitching-legacy")
+include("ges-http")
+include("subscription")
+include("subscription-legacy")
