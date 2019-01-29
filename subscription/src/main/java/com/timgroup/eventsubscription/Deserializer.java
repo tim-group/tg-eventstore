975bf8bf86556ace35d7b30a4dbb5da4a5a73992
Steve Haslam <steve.haslam@timgroup.com>
2019-01-29 15:58:25 +0000

    SteveH: Deserializer factory based on a function returning Optional

--- subscription/src/main/java/com/timgroup/eventsubscription/Deserializer.java
+++ subscription/src/main/java/com/timgroup/eventsubscription/Deserializer.java
@@ -3,6 +3,7 @@ package com.timgroup.eventsubscription;
 import com.timgroup.eventstore.api.EventRecord;
 
 import javax.annotation.Nonnull;
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -18,6 +19,21 @@ public interface Deserializer<T> {
 
     static <T> Deserializer<T> applying(Function<? super EventRecord, ? extends T> function) {
         return t -> requireNonNull(function.apply(t));
+    }
+
+    static <T> Deserializer<T> applyingOptional(Function<? super EventRecord, Optional<? extends T>> function) {
+        return new Deserializer<T>() {
+            @Nonnull
+            @Override
+            public T deserialize(EventRecord event) {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public void deserialize(EventRecord event, Consumer<? super T> consumer) {
+                function.apply(event).ifPresent(consumer);
+            }
+        };
     }
 
     static <T> Deserializer<T> filtering(Predicate<? super EventRecord> predicate, Deserializer<? extends T> downstream) {
