b287244fd1305989fedf3010b71df98db84b1633
Steve Haslam <steve.haslam@timgroup.com>
2019-04-05 12:51:17 +0100

    SteveH: support InitialCatchupFuture as an EventHandler to maybe ease transition

--- subscription/src/main/java/com/timgroup/eventsubscription/healthcheck/InitialCatchupFuture.java
+++ subscription/src/main/java/com/timgroup/eventsubscription/healthcheck/InitialCatchupFuture.java
@@ -1,12 +1,26 @@
 package com.timgroup.eventsubscription.healthcheck;
 
 import com.timgroup.eventstore.api.Position;
+import com.timgroup.eventsubscription.Event;
+import com.timgroup.eventsubscription.EventHandler;
+import com.timgroup.eventsubscription.lifecycleevents.InitialCatchupCompleted;
+import com.timgroup.eventsubscription.lifecycleevents.SubscriptionTerminated;
 
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 
-public final class InitialCatchupFuture extends CompletableFuture<Position> implements SubscriptionListener {
+public final class InitialCatchupFuture extends CompletableFuture<Position> implements SubscriptionListener, EventHandler {
+    private final EventHandler downstream;
+
+    public InitialCatchupFuture() {
+        this(EventHandler.DISCARD);
+    }
+
+    public InitialCatchupFuture(EventHandler downstream) {
+        this.downstream = downstream;
+    }
+
     @Override
     public void caughtUpAt(Position position) {
         complete(position);
@@ -21,10 +35,22 @@ public final class InitialCatchupFuture extends CompletableFuture<Position> impl
         completeExceptionally(new InitialCatchupFailedException(position, e));
     }
 
+    @Override
+    public void apply(Position position, Event deserialized) {
+        downstream.apply(position, deserialized);
+        if (deserialized instanceof InitialCatchupCompleted) {
+            complete(position);
+        }
+        else if (deserialized instanceof SubscriptionTerminated) {
+            SubscriptionTerminated subscriptionTerminated = (SubscriptionTerminated) deserialized;
+            completeExceptionally(new InitialCatchupFailedException(subscriptionTerminated.position, subscriptionTerminated.exception));
+        }
+    }
+
     public static final class InitialCatchupFailedException extends CompletionException {
         private final Position position;
 
-        InitialCatchupFailedException(Position position, Throwable cause) {
+        public InitialCatchupFailedException(Position position, Throwable cause) {
             super("Initial catchup failed at " + position, cause);
             this.position = position;
         }
