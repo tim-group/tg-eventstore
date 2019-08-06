4698907bb069662ecd51d2292d8fc6eee96bab34
Steve Haslam <steve.haslam@timgroup.com>
2019-08-06 18:08:22 +0100

    SteveH: support adding more monitoring components to an EventSource

--- api/src/main/java/com/timgroup/eventstore/api/EventSource.java
+++ api/src/main/java/com/timgroup/eventstore/api/EventSource.java
@@ -3,7 +3,11 @@ package com.timgroup.eventstore.api;
 import com.timgroup.tucker.info.Component;
 
 import javax.annotation.Nonnull;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
 
 public interface EventSource {
     @Nonnull EventReader readAll();
@@ -16,4 +20,54 @@ public interface EventSource {
     @Deprecated
     @Nonnull PositionCodec positionCodec();
     @Nonnull Collection<Component> monitoring();
+
+    default EventSource withMonitoring(Collection<Component> moreMonitoring) {
+        requireNonNull(moreMonitoring);
+
+        return new EventSource() {
+            @Nonnull
+            @Override
+            public EventReader readAll() {
+                return EventSource.this.readAll();
+            }
+
+            @Nonnull
+            @Override
+            public EventCategoryReader readCategory() {
+                return EventSource.this.readCategory();
+            }
+
+            @Nonnull
+            @Override
+            public EventStreamReader readStream() {
+                return EventSource.this.readStream();
+            }
+
+            @Nonnull
+            @Override
+            public EventStreamWriter writeStream() {
+                return EventSource.this.writeStream();
+            }
+
+            @Nonnull
+            @Override
+            public PositionCodec positionCodec() {
+                return EventSource.this.positionCodec();
+            }
+
+            @Nonnull
+            @Override
+            public Collection<Component> monitoring() {
+                List<Component> combinedMonitoring = new ArrayList<>();
+                combinedMonitoring.addAll(EventSource.this.monitoring());
+                combinedMonitoring.addAll(moreMonitoring);
+                return combinedMonitoring;
+            }
+
+            @Override
+            public String toString() {
+                return super.toString();
+            }
+        };
+    }
 }
