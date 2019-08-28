8af1e9cc1b52b006ed165663c124a10594d2a1de
Steve Haslam <steve.haslam@timgroup.com>
2019-08-28 13:18:11 +0100

    SteveH: extract Wrapper.toLegacy so it is accessible without implicit conversion

--- memory-legacy/src/main/scala/com/timgroup/eventstore/memory/Wrapper.scala
+++ memory-legacy/src/main/scala/com/timgroup/eventstore/memory/Wrapper.scala
@@ -2,19 +2,21 @@ package com.timgroup.eventstore.memory
 
 import java.util.function.{LongFunction, ToLongFunction}
 
-import com.timgroup.eventstore.api.{Position, StreamId}
+import com.timgroup.eventstore.api.{EventStore, Position, StreamId}
 import com.timgroup.eventstore.api.legacy.LegacyStore
 
 object Wrapper {
-  val toPosition: LongFunction[Position] = new LongFunction[Position] {
+  private val toPosition: LongFunction[Position] = new LongFunction[Position] {
     override def apply(value: Long): Position = JavaInMemoryEventStore.CODEC.deserializePosition(value.toString)
   }
 
-  val fromPosition: ToLongFunction[Position] = new ToLongFunction[Position] {
+  private val fromPosition: ToLongFunction[Position] = new ToLongFunction[Position] {
     override def applyAsLong(value: Position): Long = JavaInMemoryEventStore.CODEC.serializePosition(value).toLong
   }
 
+  def toLegacy(underlying : JavaInMemoryEventStore): EventStore = new LegacyStore(underlying, underlying, StreamId.streamId("all", "all"), toPosition, fromPosition)
+
   implicit class Conversion(underlying : JavaInMemoryEventStore) {
-    def toLegacy = new LegacyStore(underlying, underlying, StreamId.streamId("all", "all"), toPosition, fromPosition)
+    def toLegacy: EventStore = Wrapper.toLegacy(underlying)
   }
 }
