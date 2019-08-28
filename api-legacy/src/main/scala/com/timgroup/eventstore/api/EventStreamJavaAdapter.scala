9c326a7a5feab1e4dbd97cf5b5f6a40bb3311f27
Steve Haslam <steve.haslam@timgroup.com>
2019-08-28 12:25:17 +0100

    SteveH: rename iterator property to underlying
    avoid name clash with Scala 2.13 Iterator trait

--- api-legacy/src/main/scala/com/timgroup/eventstore/api/EventStreamJavaAdapter.scala
+++ api-legacy/src/main/scala/com/timgroup/eventstore/api/EventStreamJavaAdapter.scala
@@ -3,8 +3,9 @@ package com.timgroup.eventstore.api
 import java.util
 
 class EventStreamJavaAdapter(delegate: java.util.stream.Stream[EventInStream]) extends EventStream {
-  private val iterator: util.Iterator[EventInStream] = delegate.iterator()
-  override def hasNext: Boolean = iterator.hasNext
+  private val underlying: util.Iterator[EventInStream] = delegate.iterator()
 
-  override def next(): EventInStream = iterator.next()
+  override def hasNext: Boolean = underlying.hasNext
+
+  override def next(): EventInStream = underlying.next()
 }
