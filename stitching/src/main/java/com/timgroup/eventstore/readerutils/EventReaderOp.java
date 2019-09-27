d78126014e7b6445a66d17cd212975b86dca30c8
Steve Haslam <steve.haslam@timgroup.com>
2019-09-27 12:30:46 +0100

    SteveH: support wrapping an EventSource to include an EventReaderOp
    All the other read/write methods of the EventSource will be prohibited,
    but the monitoring passed through

--- stitching/src/main/java/com/timgroup/eventstore/readerutils/EventReaderOp.java
+++ stitching/src/main/java/com/timgroup/eventstore/readerutils/EventReaderOp.java
@@ -1,14 +1,19 @@
 package com.timgroup.eventstore.readerutils;
 
 import com.google.common.collect.ImmutableSet;
+import com.timgroup.eventstore.api.EventCategoryReader;
 import com.timgroup.eventstore.api.EventReader;
 import com.timgroup.eventstore.api.EventRecord;
 import com.timgroup.eventstore.api.EventSource;
+import com.timgroup.eventstore.api.EventStreamReader;
+import com.timgroup.eventstore.api.EventStreamWriter;
 import com.timgroup.eventstore.api.ResolvedEvent;
 import com.timgroup.eventstore.api.StreamId;
+import com.timgroup.tucker.info.Component;
 
 import javax.annotation.Nonnull;
 import java.time.Instant;
+import java.util.Collection;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -70,6 +75,41 @@ public interface EventReaderOp {
     @Nonnull
     default EventReader apply(EventSource eventSource) {
         return apply(eventSource.readAll());
+    }
+
+    @Nonnull
+    default EventSource wrapReadOnlySource(EventSource underlying) {
+        return new EventSource() {
+            @Nonnull
+            @Override
+            public EventReader readAll() {
+                return apply(underlying.readAll());
+            }
+
+            @Nonnull
+            @Override
+            public EventCategoryReader readCategory() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Nonnull
+            @Override
+            public EventStreamReader readStream() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Nonnull
+            @Override
+            public EventStreamWriter writeStream() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Nonnull
+            @Override
+            public Collection<Component> monitoring() {
+                return underlying.monitoring();
+            }
+        };
     }
 
     default EventReaderOp andThen(EventReaderOp next) {
