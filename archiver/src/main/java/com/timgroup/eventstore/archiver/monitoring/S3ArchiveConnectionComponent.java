1558d10dc167521312f29327e6672bbd9c3a3ce4
gallan <graham.allan@timgroup.com>
2019-09-30 09:53:31 +0100

    gallan: add timing into the S3ArchiveConnectionComponent.

--- archiver/src/main/java/com/timgroup/eventstore/archiver/monitoring/S3ArchiveConnectionComponent.java
+++ archiver/src/main/java/com/timgroup/eventstore/archiver/monitoring/S3ArchiveConnectionComponent.java
@@ -4,8 +4,11 @@ import com.timgroup.eventstore.archiver.S3ArchiveMaxPositionFetcher;
 import com.timgroup.tucker.info.Component;
 import com.timgroup.tucker.info.Report;
 
+import java.time.Duration;
+
 import static com.timgroup.tucker.info.Status.CRITICAL;
 import static com.timgroup.tucker.info.Status.OK;
+import static java.lang.String.format;
 
 public class S3ArchiveConnectionComponent extends Component {
     private final String eventStoreId;
@@ -19,14 +22,20 @@ public class S3ArchiveConnectionComponent extends Component {
 
     @Override
     public Report getReport() {
+        long start = System.currentTimeMillis();
         try {
             return maxPositionFetcher.maxPosition()
-                    .map(position -> new Report(OK, "Successfully connected to S3 EventStore, max position=" + position))
-                    .orElseGet(() -> new Report(CRITICAL, "Successfully connected to S3 EventStore, but no EventStore with ID='" + eventStoreId + "' exists"));
+                    .map(position -> new Report(OK, "Successfully connected to S3 EventStore, max position=" + position + ". " + timingInfo(start)))
+                    .orElseGet(() -> new Report(CRITICAL, "Successfully connected to S3 EventStore, but no EventStore with ID='" + eventStoreId + "' exists. " + timingInfo(start)));
         } catch (Exception e) {
             return new Report(CRITICAL, "Unable to connect to S3 Archive to retrieve max position\n"
-                    + ComponentUtils.getStackTraceAsString(e));
+                    + ComponentUtils.getStackTraceAsString(e)
+                    + "\n" + timingInfo(start));
 
         }
+    }
+
+    private String timingInfo(long start) {
+        return format("Took %s", Duration.ofMillis(System.currentTimeMillis() - start));
     }
 }
