931f8f04c2865abe870b5e342f7ac1a66a12c9c1
Markus Stoy <markus.stoy@acuris.com>
2020-04-01 15:57:11 +0100

    Mehul/Markus: finish first draft of FileFeeCacheEventReader
    TODO backfill unit tests

--- filesystem/file-feed-cache/src/main/java/com/timgroup/filesystem/filefeedcache/FileFeedCacheEventReader.java
+++ filesystem/file-feed-cache/src/main/java/com/timgroup/filesystem/filefeedcache/FileFeedCacheEventReader.java
@@ -12,8 +12,6 @@ import com.timgroup.eventstore.archiver.S3ArchiveKeyFormat;
 import com.timgroup.eventstore.archiver.S3ArchivePosition;
 import com.timgroup.filefeed.reading.HttpFeedCacheStorage;
 import com.timgroup.filefeed.reading.StorageLocation;
-import com.timgroup.remotefilestorage.api.RemoteFileDetails;
-import com.timgroup.remotefilestorage.s3.S3ListableStorage;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -28,14 +26,12 @@ import java.util.zip.GZIPInputStream;
 
 import static java.util.Objects.requireNonNull;
 
-public final class FileFeedCachedEventReader implements EventReader {
+public final class FileFeedCacheEventReader implements EventReader {
 
-    private final S3ListableStorage s3ListableStorage;
     private final HttpFeedCacheStorage downloadableStorage;
     private final S3ArchiveKeyFormat s3ArchiveKeyFormat;
 
-    public FileFeedCachedEventReader(S3ListableStorage s3ListableStorage, HttpFeedCacheStorage downloadableStorage, S3ArchiveKeyFormat s3ArchiveKeyFormat) {
-        this.s3ListableStorage = s3ListableStorage;
+    public FileFeedCacheEventReader(HttpFeedCacheStorage downloadableStorage, S3ArchiveKeyFormat s3ArchiveKeyFormat) {
         this.downloadableStorage = downloadableStorage;
         this.s3ArchiveKeyFormat = s3ArchiveKeyFormat;
     }
@@ -44,47 +40,47 @@ public final class FileFeedCachedEventReader implements EventReader {
     public Stream<ResolvedEvent> readAllForwards(Position positionExclusive) {
         S3ArchivePosition toReadFrom = (S3ArchivePosition) requireNonNull(positionExclusive);
 
-        return listAllBatches()
+        return listFeedFiles()
                 .filter(batchesEndingWithPositionGreaterThan(toReadFrom))
                 .flatMap(this::getEventsFromMultiTry)
                 .filter(fromPosition(toReadFrom))
                 .map(this::toResolvedEvent);
     }
 
-    private Stream<RemoteFileDetails> listAllBatches() {
-        return s3ListableStorage.list(s3ArchiveKeyFormat.eventStorePrefix(), null);
+    private Stream<String> listFeedFiles() {
+        return downloadableStorage.list(StorageLocation.TimGroupEventStoreFeedStore, s3ArchiveKeyFormat.eventStorePrefix()).stream();
     }
 
     private Predicate<EventStoreArchiverProtos.Event> fromPosition( S3ArchivePosition toReadFrom) {
         return (event) -> event.getPosition() >= toReadFrom.value;
     }
 
-    private Predicate<RemoteFileDetails> batchesEndingWithPositionGreaterThan( S3ArchivePosition toReadFrom) {
-        return (batchFile) -> s3ArchiveKeyFormat.positionValueFrom(batchFile.name) >= toReadFrom.value;
+    private Predicate<String> batchesEndingWithPositionGreaterThan( S3ArchivePosition toReadFrom) {
+        return (fileName) -> s3ArchiveKeyFormat.positionValueFrom(fileName) >= toReadFrom.value;
     }
 
-    private Stream<EventStoreArchiverProtos.Event> getEventsFromMultiTry(RemoteFileDetails remoteFileDetails) {
+    private Stream<EventStoreArchiverProtos.Event> getEventsFromMultiTry(String fileName) {
         int maxAttempts = 5;
         int attemptsSoFar = 0;
         Optional<Exception> lastException = Optional.empty();
         while(attemptsSoFar < maxAttempts) {
             attemptsSoFar += 1;
             try {
-                return loadEventMessages(remoteFileDetails).stream();
+                return loadEventMessages(fileName).stream();
             } catch(Exception e) {
                 lastException = Optional.of(e);
             }
         }
-        throw new RuntimeException(String.format("Failed to download S3 file %s after %s attempts. Giving up! ", remoteFileDetails.name, attemptsSoFar), lastException.get());
+        throw new RuntimeException(String.format("Failed to download S3 file %s after %s attempts. Giving up! ", fileName, attemptsSoFar), lastException.get());
 
     }
 
 
-    private List<EventStoreArchiverProtos.Event> loadEventMessages(RemoteFileDetails remoteFileDetails) {
+    private List<EventStoreArchiverProtos.Event> loadEventMessages(String fileName) {
         StorageLocation timGroupEventStoreFeedStore = StorageLocation.TimGroupEventStoreFeedStore;
-        Optional<org.joda.time.Instant> arrivalTime = downloadableStorage.getArrivalTime(timGroupEventStoreFeedStore, remoteFileDetails.name);
+        Optional<org.joda.time.Instant> arrivalTime = downloadableStorage.getArrivalTime(timGroupEventStoreFeedStore, fileName);
         if (arrivalTime.isPresent()) {
-            try (InputStream inputStream = downloadableStorage.get(timGroupEventStoreFeedStore, remoteFileDetails.name)) {
+            try (InputStream inputStream = downloadableStorage.get(timGroupEventStoreFeedStore, fileName)) {
                 return parseEventMessages(inputStream);
             } catch (Exception e) {
                 throw new RuntimeException(e);
@@ -123,7 +119,7 @@ public final class FileFeedCachedEventReader implements EventReader {
 
     @Override
     public Optional<ResolvedEvent> readLastEvent() {
-        return listAllBatches()
+        return listFeedFiles()
                 .reduce((r1, r2) -> r2)
                 .flatMap(file -> lastElementOf(loadEventMessages(file)))
                 .map(this::toResolvedEvent);
--- filesystem/file-feed-cache/src/test/java/com/timgroup/filesystem/filefeedcache/ReadingMDEventsFromFileFeedCache.java
+++ filesystem/file-feed-cache/src/test/java/com/timgroup/filesystem/filefeedcache/ReadingMDEventsFromFileFeedCache.java
@@ -1,17 +1,11 @@
 package com.timgroup.filesystem.filefeedcache;
 
-import com.amazonaws.services.s3.AmazonS3;
 import com.timgroup.eventstore.api.EventReader;
 import com.timgroup.eventstore.archiver.S3ArchiveKeyFormat;
 import com.timgroup.eventstore.merging.MergedEventReader;
 import com.timgroup.eventstore.merging.MergingStrategy;
 import com.timgroup.eventstore.merging.NamedReaderWithCodec;
 import com.timgroup.filefeed.reading.HttpFeedCacheStorage;
-import com.timgroup.remotefilestorage.api.RemoteFileDetails;
-import com.timgroup.remotefilestorage.s3.S3ClientFactory;
-import com.timgroup.remotefilestorage.s3.S3ListableStorage;
-import com.timgroup.structuredevents.Slf4jEventSink;
-import org.slf4j.LoggerFactory;
 
 import java.io.PrintWriter;
 import java.net.URI;
@@ -19,40 +13,22 @@ import java.time.Clock;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import java.util.List;
-import java.util.Properties;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
 
 public class ReadingMDEventsFromFileFeedCache {
     private static AtomicLong eventCount = new AtomicLong(0);
 
     public static void main(String[] args) {
-        Properties properties = new Properties();
-        properties.setProperty("s3.accessKey", "KEY");
-        properties.setProperty("s3.secretKey", "SECRET");
-        properties.setProperty("s3.region", "eu-west-2");
-
-        AmazonS3 client = new S3ClientFactory(new Slf4jEventSink(LoggerFactory.getLogger(ReadingMDEventsFromFileFeedCache.class.getName()))).fromProperties(properties);
-        String bucketName = "production-tg-eventstore-archive";
-        S3ListableStorage listableStorage = new S3ListableStorage(client, bucketName, Integer.MAX_VALUE);
         HttpFeedCacheStorage downloadableStorage = new HttpFeedCacheStorage(URI.create("http://latest-file-feed-cacheapp-vip.oy.net.local:8000"));
 
-
-        List<RemoteFileDetails> list = listableStorage.list("marketdata-event/", null).collect(Collectors.toList());
-        list.stream().limit(5).forEach(System.out::println);
-
-
-        NamedReaderWithCodec marketdataEventReader = eventReaderFor(new FileFeedCachedEventReader(listableStorage, downloadableStorage, new S3ArchiveKeyFormat("marketdata-event")), "marketdata-event");
-        FileFeedCachedEventReader imeEventReader = new FileFeedCachedEventReader(listableStorage, downloadableStorage, new S3ArchiveKeyFormat("tradeideasmonitor-Event"));
+        NamedReaderWithCodec marketdataEventReader = eventReaderFor(new FileFeedCacheEventReader(downloadableStorage, new S3ArchiveKeyFormat("marketdata-event")), "marketdata-event");
+        FileFeedCacheEventReader imeEventReader = new FileFeedCacheEventReader(downloadableStorage, new S3ArchiveKeyFormat("tradeideasmonitor-Event"));
         NamedReaderWithCodec timIMEEventReader = eventReaderFor(imeEventReader, "tradeideasmonitor-Event");
 
         MergedEventReader eventReader = new MergedEventReader(Clock.systemUTC(), (MergingStrategy<Instant>) event ->
                 event.eventRecord().timestamp(), marketdataEventReader, timIMEEventReader);
 
         long start = System.currentTimeMillis();
-
-        System.out.println(properties);
 
         try (PrintWriter printWriter = new PrintWriter("s3eventstoretimes.csv")) {
             printWriter.println("position,time(s)");
