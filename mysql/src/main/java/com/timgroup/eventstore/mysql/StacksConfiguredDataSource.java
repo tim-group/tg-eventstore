08277cae5b34cb1d1164c954bb1d3a43ad1e3627
mpimm <max.pimm@acuris.com>
2020-07-27 15:07:12 +0200

    mpimm: Add prometheus metrics for the datasource
    Deprecate dropwizard API's

--- mysql/src/main/java/com/timgroup/eventstore/mysql/StacksConfiguredDataSource.java
+++ mysql/src/main/java/com/timgroup/eventstore/mysql/StacksConfiguredDataSource.java
@@ -4,12 +4,12 @@ import com.codahale.metrics.MetricRegistry;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import com.mchange.v2.c3p0.PooledDataSource;
 import com.typesafe.config.Config;
+import io.prometheus.client.Gauge;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 import java.sql.SQLException;
-import java.util.Optional;
 import java.util.Properties;
 
 import static java.lang.String.format;
@@ -18,59 +18,65 @@ public final class StacksConfiguredDataSource {
 
     private static final Logger logger = LoggerFactory.getLogger(StacksConfiguredDataSource.class);
 
+    private static final Gauge databaseConnections = Gauge.build("database_connections", "TG Eventstore Database connections")
+            .labelNames("database", "state")
+            .register();
+
     public static final int DEFAULT_MAX_POOLSIZE = 15;
     public static final int DEFAULT_SOCKET_TIMEOUT_MS = 15000;
 
     private StacksConfiguredDataSource() { /* prevent instantiation */ }
 
-    /**
-     * @deprecated  replaced by {@link #pooledMasterDb(Properties properties, String configPrefix, MetricRegistry metricRegistry)}
-     */
-    @Deprecated
     public static PooledDataSource pooledMasterDb(Properties properties, String configPrefix) {
         return pooledMasterDb(properties, configPrefix, null);
     }
 
+    @Deprecated
     public static PooledDataSource pooledMasterDb(Properties properties, String configPrefix, @Nullable MetricRegistry metricRegistry) {
         return pooledMasterDb(properties, configPrefix, DEFAULT_MAX_POOLSIZE, metricRegistry);
     }
 
-    /**
-     * @deprecated  replaced by {@link #pooledMasterDb(Properties properties, String configPrefix, int maxPoolSize, MetricRegistry metricRegistry)}
-     */
-    @Deprecated
     public static PooledDataSource pooledMasterDb(Properties properties, String configPrefix, int maxPoolSize) {
         return pooledMasterDb(properties, configPrefix, maxPoolSize, null);
     }
 
+    @Deprecated
     public static PooledDataSource pooledMasterDb(Properties properties, String configPrefix, int maxPoolSize, @Nullable MetricRegistry metricRegistry) {
         return getPooledDataSource(properties, configPrefix, "hostname", maxPoolSize, DEFAULT_SOCKET_TIMEOUT_MS, metricRegistry);
     }
 
-    /**
-     * @deprecated  replaced by {@link #pooledReadOnlyDb(Properties properties, String configPrefix, MetricRegistry metricRegistry)}
-     */
-    @Deprecated
     public static PooledDataSource pooledReadOnlyDb(Properties properties, String configPrefix) {
         return pooledReadOnlyDb(properties, configPrefix, null);
     }
 
+    /**
+     * @deprecated Use corresponding API without metric registry
+     */
+    @Deprecated
     public static PooledDataSource pooledReadOnlyDb(Properties properties, String configPrefix, @Nullable MetricRegistry metricRegistry) {
         return pooledReadOnlyDb(properties, configPrefix, DEFAULT_MAX_POOLSIZE, metricRegistry);
     }
 
-    /**
-     * @deprecated  replaced by {@link #pooledReadOnlyDb(Properties properties, String configPrefix, int maxPoolSize, MetricRegistry metricRegistry)}
-     */
-    @Deprecated
     public static PooledDataSource pooledReadOnlyDb(Properties properties, String configPrefix, int maxPoolSize) {
         return pooledReadOnlyDb(properties, configPrefix, maxPoolSize, null);
     }
 
+    /**
+     * @deprecated Use corresponding API without metric registry
+     */
+    @Deprecated
     public static PooledDataSource pooledReadOnlyDb(Properties properties, String configPrefix, int maxPoolSize, @Nullable MetricRegistry metricRegistry) {
         return pooledReadOnlyDb(properties, configPrefix, maxPoolSize, DEFAULT_SOCKET_TIMEOUT_MS, metricRegistry);
     }
 
+    public static PooledDataSource pooledReadOnlyDb(Properties properties, String configPrefix, int maxPoolSize, int socketTimeoutMs) {
+        return getPooledDataSource(properties, configPrefix, "read_only_cluster", maxPoolSize, socketTimeoutMs, null);
+    }
+
+    /**
+     * @deprecated Use corresponding API without metric registry
+     */
+    @Deprecated
     public static PooledDataSource pooledReadOnlyDb(Properties properties, String configPrefix, int maxPoolSize, int socketTimeoutMs, @Nullable MetricRegistry metricRegistry) {
         return getPooledDataSource(properties, configPrefix, "read_only_cluster", maxPoolSize, socketTimeoutMs, metricRegistry);
     }
@@ -98,26 +104,26 @@ public final class StacksConfiguredDataSource {
         );
     }
 
-    /**
-     * @deprecated  replaced by {@link #pooledMasterDb(Config, MetricRegistry)}
-     */
-    @Deprecated
     public static PooledDataSource pooledMasterDb(Config config) {
         return pooledMasterDb(config, DEFAULT_MAX_POOLSIZE, null);
     }
 
+    /**
+     * @deprecated Use corresponding API without metric registry
+     */
+    @Deprecated
     public static PooledDataSource pooledMasterDb(Config config, @Nullable MetricRegistry metricRegistry) {
         return pooledMasterDb(config, DEFAULT_MAX_POOLSIZE, metricRegistry);
     }
 
-    /**
-     * @deprecated  replaced by {@link #pooledMasterDb(Config, int, MetricRegistry)}
-     */
-    @Deprecated
     public static PooledDataSource pooledMasterDb(Config config, int maxPoolSize) {
         return pooledMasterDb(config, maxPoolSize, null);
     }
 
+    /**
+     * @deprecated Use corresponding API without metric registry
+     */
+    @Deprecated
     public static PooledDataSource pooledMasterDb(Config config, int maxPoolSize, @Nullable MetricRegistry metricRegistry) {
         return pooled(
                 config.getString("hostname"),
@@ -132,26 +138,26 @@ public final class StacksConfiguredDataSource {
         );
     }
 
-    /**
-     * @deprecated  replaced by {@link #pooledReadOnlyDb(Config, MetricRegistry)}
-     */
-    @Deprecated
     public static PooledDataSource pooledReadOnlyDb(Config config) {
         return pooledReadOnlyDb(config, null);
     }
 
+    /**
+     * @deprecated Use corresponding API without metric registry
+     */
+    @Deprecated
     public static PooledDataSource pooledReadOnlyDb(Config config, @Nullable MetricRegistry metricRegistry) {
         return pooledReadOnlyDb(config, DEFAULT_MAX_POOLSIZE, metricRegistry);
     }
 
-    /**
-     * @deprecated  replaced by {@link #pooledReadOnlyDb(Config, int, MetricRegistry)}
-     */
-    @Deprecated
     public static PooledDataSource pooledReadOnlyDb(Config config, int maxPoolSize) {
         return pooledReadOnlyDb(config, maxPoolSize, null);
     }
 
+    /**
+     * @deprecated Use corresponding API without metric registry
+     */
+    @Deprecated
     public static PooledDataSource pooledReadOnlyDb(Config config, int maxPoolSize, @Nullable MetricRegistry metricRegistry) {
         return pooled(
                 config.getString("read_only_cluster"),
@@ -175,8 +181,7 @@ public final class StacksConfiguredDataSource {
             String driver,
             int maxPoolsize,
             int socketTimeoutMs,
-            @Nullable MetricRegistry metricRegistry)
-    {
+            @Nullable MetricRegistry metricRegistry) {
         ComboPooledDataSource dataSource = new ComboPooledDataSource();
         dataSource.setJdbcUrl(format("jdbc:mysql://%s:%d/%s?rewriteBatchedStatements=true&secondsBeforeRetryMaster=1&queriesBeforeRetryMaster=1&connectTimeout=5000&socketTimeout=" + socketTimeoutMs,
                 hostname,
@@ -193,7 +198,11 @@ public final class StacksConfiguredDataSource {
         dataSource.setMaxConnectionAge(60);
         dataSource.setPreferredTestQuery("SELECT 1");
 
-        configureToSendMetrics(dataSource, database, metricRegistry);
+        if (metricRegistry == null) {
+            collectPrometheusMetrics(dataSource, database);
+        } else {
+            configureGraphiteMetrics(dataSource, database, metricRegistry);
+        }
 
         try {
             Class.forName(driver);
@@ -203,16 +212,14 @@ public final class StacksConfiguredDataSource {
         return dataSource;
     }
 
-    private static void configureToSendMetrics(PooledDataSource dataSource, String databaseName, @Nullable MetricRegistry optionalMetricRegistry) {
-        Optional.ofNullable(optionalMetricRegistry).ifPresent(metricRegistry -> {
-            sendTo(metricRegistry, databaseName, "activeConnections", dataSource::getNumBusyConnectionsDefaultUser);
-            sendTo(metricRegistry, databaseName, "idleConnections", dataSource::getNumIdleConnectionsDefaultUser);
-            sendTo(metricRegistry, databaseName, "totalConnections", dataSource::getNumConnectionsDefaultUser);
-            sendTo(metricRegistry, databaseName, "unclosedOrphanedConnections", dataSource::getNumUnclosedOrphanedConnectionsDefaultUser);
-        });
+    private static void configureGraphiteMetrics(PooledDataSource dataSource, String databaseName, MetricRegistry metricRegistry) {
+        sendCollectionState(metricRegistry, databaseName, "activeConnections", dataSource::getNumBusyConnectionsDefaultUser);
+        sendCollectionState(metricRegistry, databaseName, "idleConnections", dataSource::getNumIdleConnectionsDefaultUser);
+        sendCollectionState(metricRegistry, databaseName, "totalConnections", dataSource::getNumConnectionsDefaultUser);
+        sendCollectionState(metricRegistry, databaseName, "unclosedOrphanedConnections", dataSource::getNumUnclosedOrphanedConnectionsDefaultUser);
     }
 
-    private static void sendTo(MetricRegistry metrics, String databaseName, String name, DataSourceMetric source) {
+    private static void sendCollectionState(MetricRegistry metrics, String databaseName, String name, DataSourceMetric source) {
         metrics.gauge(String.format("database.%s.dataSource.%s.value", databaseName, name), () -> () -> {
             try {
                 return source.get();
@@ -221,6 +228,27 @@ public final class StacksConfiguredDataSource {
                 return null;
             }
         });
+    }
+
+    private static void collectPrometheusMetrics(PooledDataSource dataSource, String databaseName) {
+        collectConnectionState(databaseName, "activeConnections", dataSource::getNumBusyConnectionsDefaultUser);
+        collectConnectionState(databaseName, "idleConnections", dataSource::getNumIdleConnectionsDefaultUser);
+        collectConnectionState(databaseName, "totalConnections", dataSource::getNumConnectionsDefaultUser);
+        collectConnectionState(databaseName, "unclosedOrphanedConnections", dataSource::getNumUnclosedOrphanedConnectionsDefaultUser);
+    }
+
+    private static void collectConnectionState(String databaseName, String connectionState, DataSourceMetric source) {
+        databaseConnections.setChild(new Gauge.Child() {
+            @Override
+            public double get() {
+                try {
+                    return source.get();
+                } catch (SQLException e) {
+                    logger.warn("Failed to fetch " + connectionState + " metric from database " + databaseName, e);
+                    return -1;
+                }
+            }
+        }, databaseName, connectionState);
     }
 
     @FunctionalInterface
